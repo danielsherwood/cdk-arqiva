2a3ef0c9b7bbf3d640def26de02f1f3f09f5965e
Daniel Sherwood <d_j_sherwood1978@yahoo.co.uk>
2025-05-23 12:01:47 +0100

    Add in hashed out s3 code in the macro.

--- lambda/hello.js
+++ lambda/hello.js
@@ -1,4 +1,16 @@
+const AWS = require('aws-sdk');
+
 exports.handler = async (event) => {
+    const bucketName = process.env.bucketName || '';
+    const objectKey = 'input-file.txt';
+    // S3 bucket
+    /*
+    const s3 = new AWS.S3();
+    const params = { Bucket: bucketName, Key: objectKey };
+    const response = await s3.getObject(params).promise();
+    const dynamic_string = response.Body?.toString('utf-8') || 'dynamic string';
+    */
+    // Needs deleting when find out how to properly include S3 in this file
     let dynamic_string = 'dynamic string';
     let saved_string = `<h1>The saved string is a ${dynamic_string}</h1>`;
     return {
